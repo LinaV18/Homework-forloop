5fe1e564243c93df57f117d80a472d2ac9630efb
Lina Valand <linavaland@hotmail.co.uk>
2022-08-05 21:10:42 +0100

    forloop

---
+++ Forlooptable10.java
@@ -0,0 +1,14 @@
+package Forloop;
+
+public class Forlooptable10 {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(num * i);
+        }
+    }
+
+}
